19ac1418394b617c8a9d8ecd7b108973ae0e8410
Riivo Meremaa <riivo.meremaa@voco.ee>
2026-02-17 12:52:29 +0200

    Fax

---
+++ Beginning powershell script part 3.ps1
@@ -0,0 +1,44 @@
+﻿Write-Host "It is a good day" -ForegroundColor Green
+
+Write-Host "It is a good day" -ForgeroundColor Yellow
+
+Write-Host "It is a good day"
+
+Write-Host "It is a good day" -ForegroundColor Cyan 
+
+$i = 10
+$j = 5
+if ($i -gt $j)
+{
+Write-Host "$i is greater than $j"
+}
+else
+{
+Write-Host "$j is greater than $i"
+}
+
+Get-Service -Name Spooler
+
+$service = Get-Service Spooler | Select Status
+
+if($service.Status -eq "Running")
+ {
+  Write-Host "Service is Good" -ForegroundColor Green
+ }
+ elseif($service.status -eq "Stopped")
+ {
+  Write-Host "Service is Bad" -ForegroundColor Red
+ }
+
+Get-Service Fax
+
+$service = Get-Service Fax | Select Status
+
+if($service.Status -eq "Running")
+ {
+  Write-Host "Service is Good" -ForegroundColor Green
+ }
+ elseif($service.status -eq "Stopped")
+ {
+  Write-Host "Service is Bad" -ForegroundColor Red
+ }
