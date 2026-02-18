fb9b34e8687284c8478f6f315d340dda928246af
Riivo Meremaa <riivo.meremaa@voco.ee>
2026-02-18 13:25:07 +0200

    Practice 9

---
+++ Practice 9.ps1
@@ -0,0 +1,27 @@
+﻿#Do While Loop
+
+Write-Host "Do While Loop" -ForegroundColor Red
+
+$i = 0
+do
+{
+    Write-Host "Current Value is : " $i -ForegroundColor Cyan
+    $i++
+}while($i -lt 3)
+
+
+$count = 0
+
+do {
+    $process = Get-Process -Name notepad -ErrorAction SilentlyContinue
+    
+    if ($process) {
+        Write-Host "Notepad is running"
+        $count++
+        Start-Sleep -Seconds 1
+    }
+
+} while ($process)
+
+Write-Host "All Notepad instances are closed."
+Write-Host "The statement was displayed $count times."
