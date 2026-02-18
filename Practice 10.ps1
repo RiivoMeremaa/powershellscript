a2f7766ab4e6ed570282d08a609d79cfe3ae5fac
Riivo Meremaa <riivo.meremaa@voco.ee>
2026-02-18 13:32:04 +0200

    Notepad

---
+++ Practice 10.ps1
@@ -0,0 +1,27 @@
+﻿#Do Untill Loop
+
+Write-Host "Do Untill Loop" -ForegroundColor Red
+
+$i = 0
+do
+{
+    Write-Host "Current Value is : " $i -ForegroundColor Cyan
+    $i++
+}until($i -ge 3)
+
+
+#Practice 10
+
+Write-Host "Practice 10" -ForegroundColor Red
+
+$count = 0
+
+do {
+    Write-Host "Notepad is running"
+    $count++
+    Start-Sleep -Seconds 1
+}
+until (-not (Get-Process -Name notepad -ErrorAction SilentlyContinue))
+
+Write-Host "All Notepad instances are closed."
+Write-Host "The statement was displayed $count times."
