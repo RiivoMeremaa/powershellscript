8aa5b78e62639c25d3f76dc90a7353a5e4b5fc2e
Riivo Meremaa <riivo.meremaa@voco.ee>
2026-02-18 13:21:51 +0200

    Loops

---
+++ Practice 8.ps1
@@ -0,0 +1,19 @@
+﻿#While loop
+Write-Host "While loop" -ForegroundColor Red
+
+$i = 0
+while($i -lt 3)
+{
+    Write-Host "Current Value is : " $i -ForegroundColor Yellow
+    $i++
+}
+
+#Practice 8
+
+while (Get-Process -Name notepad -ErrorAction SilentlyContinue)
+{
+    Write-Host "Notepad is running"
+    Start-Sleep -Seconds 2
+}
+
+Write-Host "All Notepad instances are closed. Loop ended."
