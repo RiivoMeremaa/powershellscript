b387ee137af29d1a5a19a47cc9ebfd1f6455bad6
Riivo Meremaa <riivo.meremaa@voco.ee>
2026-02-25 09:02:50 +0200

    Notepad

---
+++ Practice 5.ps1
@@ -0,0 +1,20 @@
+﻿#Write-Host "where/Where-Object" -ForegroundColor Blue
+
+#Get-Service | where{$_.status -eq "Stopped"}
+
+#Get-service | where{$_.status -eq "Stopped" -and $_.name -like "A*"}
+
+#(Get-Service).where({$_.name -like "A*"})
+
+#Practice 5
+
+Write-Host "Pracitce 5" -ForegroundColor Red
+
+Get-Process | where{$_.ProcessName -eq "notepad"} | Select ProcessName,Id
+
+# Teine ülesanne 
+
+$file = Get-ChildItem -Path "C:\temp\test" -Filter "*.csv" | Select-Object -First 1
+Write-Host "`nFileName : $($file.Name)"
+Write-Host "Size in KB : $($file.Length / 1KB)"
+Write-Host "Size in MB : $($file.Length / 1MB)"
